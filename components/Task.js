866d82dd0c8f3d6970adefc86f94ddef7af9b125
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-05-21 01:50:41 -0700

    Modifying task component

--- components/Task.js
+++ components/Task.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ProgressViewIOSComponent,
+} from "react-native";
 
-const Task = () => {
+const Task = (props) => {
   return (
-    <View>
-      <Text>This is a task</Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+      <View style={styles.circular}></View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  item: {},
+  itemLeft: {},
+  square: {},
+  itemText: {},
+  circular: {},
 });
 
 export default Task;
